e4fd80e1b8e52891a48c356cfbbae610e3befb26
yutaka1211 <star12wars11@hotmail.co.jp>
2020-07-21 10:27:54 +0900

    Update union_find.c

--- union_find/union_find.c
+++ union_find/union_find.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-#define N_MAX (1000)                    //従業員の数
+#define N_MAX (1000)                                                            //従業員の数
 
 int p[N_MAX];
 
 /*所属するトップの番号を返す*/
 int Find(int x){
-    if(p[x]==x){ return x; }            //自分がトップである場合
-    else { return p[x]=Find(p[x]); }    //自分の上司が存在する場合
+    if(p[x]==x){ return x; }                                                    //自分がトップである場合
+    else { return p[x]=Find(p[x]); }                                            //自分の上司が存在する場合
 }
 
 
 void Same(int x, int y){
-    int n=Find(x);
-    int m=Find(y);
+    int n=Find(x);                                                              //xの最高位の上司をnに格納
+    int m=Find(y);                                                              //yの最高位の上司をmに格納
 
-    if(n==m){ printf("%d & %d are in the same organization.\n", x, y); }
-    else { printf("%d & %d are in the different organization.\n", x, y); }
+    if(n==m){ printf("%d & %d are in the same organization.\n", x, y); }        //xとyが同じ所属である場合
+    else { printf("%d & %d are in the different organization.\n", x, y); }      //xとyが違う所属である場合
 }
 
 int main(void){
@@ -23,8 +23,8 @@ int main(void){
     int n, i, j, sv;
     fp=fopen("input.txt", "r");
     fscanf(fp, "%d", &n);
-    for(i=0;i<n;i++){ p[i]=i; }         //自分の番号を配列に格納
-    while(fscanf(fp, "%d %d", &i, &j)!=EOF){ p[i]=sv; }
+    for(i=0;i<n;i++){ p[i]=i; }                                                  //自分の番号を配列pに格納
+    while(fscanf(fp, "%d %d", &i, &j)!=EOF){ p[i]=sv; }                          //上司を配列pに格納
 
     fclose(fp);
 
